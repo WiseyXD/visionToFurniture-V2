68f8149ac34d409a806146294388d654d550ca5f
WiseyXD <aryan.s.nag@gmail.com>
2024-07-22 21:40:29 +0530

    fix : ui landing page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,6 +5,11 @@ import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import ModernSofa from '@/public2/landing-page-1-product-1-sofa.jpg';
+import ImportantChair from '@/public2/landing-page-1-product-2-chair.jpg';
+import CoffeTable from '@/public2/landing-page-1-product-3-coffee-table.jpg';
+import Landing1Top from '@/public2/landing-page-1-top.jpg';
+
 export default function Home() {
     const t = useTranslations('HomePage');
     return (
@@ -43,7 +48,8 @@ export default function Home() {
                             </div>
                         </div>
                         <Image
-                            src="/landing-page-1-top.jpg"
+                            // src="/landing-page-1-top.jpg"
+                            src={Landing1Top}
                             width="550"
                             height="550"
                             alt="Hero"
@@ -74,7 +80,7 @@ export default function Home() {
                         <Card>
                             <CardContent className="mt-4">
                                 <Image
-                                    src="/landing-page-1-product-1-sofa.jpg"
+                                    src={ModernSofa}
                                     width="300"
                                     height="300"
                                     alt="Product"
@@ -99,7 +105,7 @@ export default function Home() {
                         <Card>
                             <CardContent className="mt-4">
                                 <Image
-                                    src="/landing-page-1-product-2-chair.jpg"
+                                    src={ImportantChair}
                                     width="300"
                                     height="300"
                                     alt="Product"
@@ -124,7 +130,7 @@ export default function Home() {
                         <Card>
                             <CardContent className="mt-4">
                                 <Image
-                                    src="/landing-page-1-product-3-coffee-table.jpg"
+                                    src={CoffeTable}
                                     width="300"
                                     height="300"
                                     alt="Product"
@@ -362,85 +368,6 @@ export default function Home() {
     );
 }
 
-// import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-// import { Card, CardContent } from '@/components/ui/card';
-// import { useTranslations } from 'next-intl';
-// import Image from 'next/image';
-// import Link from 'next/link';
-
-// export default function Home() {
-//     const t = useTranslations('HomePage');
-//     return (
-//         <div>
-//             <section className="w-full py-12 md:py-24 lg:py-32">
-//                 <div className="container px-4 md:px-6">
-//                     <div className="grid gap-6 lg:grid-cols-[1fr_550px] lg:gap-12 xl:grid-cols-[1fr_650px]">
-//                         <div className="flex flex-col justify-center space-y-4">
-//                             <div className="space-y-2">
-//                                 <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl xl:text-7xl">
-//                                     {t('title')}
-//                                 </h1>
-//                                 <p className="max-w-[700px] text-muted-foreground md:text-xl">
-//                                     {t('head')}
-//                                 </p>
-//                             </div>
-//                             <div className="flex flex-col gap-2 min-[400px]:flex-row">
-//                                 <Link
-//                                     href="https://vision2furniture-3d-models.netlify.app/"
-//                                     className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 button-hover-effect"
-//                                     prefetch={false}
-//                                 >
-//                                     {t('try')}
-//                                 </Link>
-//                                 <Link
-//                                     href="#"
-//                                     className="inline-flex h-10 items-center justify-center rounded-md border border-input bg-background px-8 text-sm font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 button-hover-effect"
-//                                     prefetch={false}
-//                                 >
-//                                     {t('learn')}
-//                                 </Link>
-//                             </div>
-//                         </div>
-//                         <Image
-//                             src="/main.jpg"
-//                             width="550"
-//                             height="550"
-//                             alt="Hero"
-//                             className="mx-auto aspect-square overflow-hidden rounded-xl object-cover hover-effect"
-//                         />
-//                     </div>
-//                 </div>
-//             </section>
-
-//             <section className="w-full py-12 md:py-24 lg:py-15 bg-muted">
-//                 <div className="container space-y-12 px-4 md:px-6">
-//                     <div className="flex flex-col items-center justify-center space-y-4 text-center">
-//                         <div className="space-y-2">
-//                             <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm">
-//                                 {t('feature1')}
-//                             </div>
-//                             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-//                                 {t('discover1')}
-//                             </h2>
-//                             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-//                                 {t('browse1')}
-//                             </p>
-//                         </div>
-//                     </div>
-//                     <div className="grid gap-6 lg:grid-cols-3 lg:gap-12">
-//                         <Card className="bg-muted hover:shadow-2xl  transform transition-transform duration-300">
-//                             <CardContent>
-//                                 <Image
-//                                     src="/Img_gen.png"
-//                                     width="330"
-//                                     height="330"
-//                                     alt="Product"
-//                                     className="mx-auto mt-5 aspect-square overflow-hidden object-cover rounded-lg hover-effect"
-//                                 />
-//                                 <div className="mt-4 space-y-1 text-center">
-//                                     <h3 className="text-lg font-bold">
-//                                         {t('modern')}
-//                                     </h3>
 //                                 </div>
 //                             </CardContent>
 //                         </Card>
